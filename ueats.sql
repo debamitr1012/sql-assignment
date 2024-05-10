f9b0f25b1efd17f18f386dae6a9110a5680a08bf
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2024-05-10 23:07:16 +0530

    sql

---
+++ ueats.sql
@@ -0,0 +1,9 @@
+SELECT *
+FROM `arboreal-vision-339901.take_home_v2.virtual_kitchen_ubereats_hours`
+LIMIT 5;
+SELECT
+      JSON_EXTRACT(value, "$.regularHours.endTime") AS end_time,  
+      JSON_EXTRACT(value, "$.regularHours.startTime") AS start_time
+FROM 
+  `arboreal-vision-339901.take_home_v2.virtual_kitchen_ubereats_hours`,
+  UNNEST(JSON_QUERY_ARRAY(response, '$.data.menus.sections')) as value
